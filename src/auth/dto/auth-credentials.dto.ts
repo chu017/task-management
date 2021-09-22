265d9dad52f71c6ab878ee959fd124e5c0f0b224
Calvin Hu <31859826+chu017@users.noreply.github.com>
2021-09-22 07:25:59 -0700

    error handling_username conflicts

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,9 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class AuthCredentialsDto {
   @ApiProperty({ example: 'user1' })
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
-  @ApiProperty({ example: 'password' })
+  @ApiProperty({ example: 'Password1' })
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'password is too weak, it need contain at least 1 upper case letter, 1 lower case letter, and 1 number or special character',
+  })
   password: string;
 }
